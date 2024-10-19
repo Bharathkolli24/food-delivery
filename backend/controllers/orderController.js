cf692ca05d5e684e42e4d8464d2b7529708995a1
Bharathkolli24 <bharathkolli24@gmail.com>
2024-10-20 00:17:15 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -9,7 +9,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 // placing user order from frontend
 const placeOrder = async (req,res) => {
 
-    const frontend_url = 'http://localhost:5173'
+    const frontend_url = 'https://food-delivery-frontend-35qj.onrender.com/'
 
     try {
         const userId = req.body.userId;
