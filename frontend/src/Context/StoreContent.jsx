687d0e74182c14d7575fd6007d08b4e6fad3f32b
Bharathkolli24 <bharathkolli24@gmail.com>
2025-09-14 17:16:50 +0530

    Update frontend code

--- frontend/src/Context/StoreContent.jsx
+++ frontend/src/Context/StoreContent.jsx
@@ -1,12 +1,11 @@
-import { createContext, useEffect, useState } from "react";
 import axios from 'axios';
+import { createContext, useEffect, useState } from "react";
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
     const [cartItems,setCartItems] = useState({});
-    // const url = 'https://food-delivery-backend-bkny.onrender.com'
-    const url = 'http://localhost:4000'
+    const url = 'https://food-delivery-backend-bkny.onrender.com'
     const [token,setToken] = useState("")
     const [food_list,setFoodList] = useState([])
     const [isLoading, setIsLoading] = useState(true);
